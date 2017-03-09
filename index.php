c4faec6cd2e3f7033a1684fcef5d3000b3ac983c
Strofe <c.sonneck@gmail.com>
2017-03-09 23:24:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,6 @@
 	</style>
 </head>
 <body>
-	Par Strofe
 	<div align="center">
 	<iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;hl=fr&amp;bgcolor=%23FFFFFF&amp;src=c.sonneck%40gmail.com&amp;color=%23691426&amp;ctz=Europe%2FParis" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 	</div>
